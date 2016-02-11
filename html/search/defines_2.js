7acd787ee1303c11e508713a336de125d3a907dd
Ben Stuart <mail@bstuart.de>
2016-02-11 15:48:26 +0100

    docs

---
+++ html/search/defines_2.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['malloc',['malloc',['../util_8h.html#a535b58ab5aa48e2e86073e334d43fd32',1,'util.h']]],
+  ['memmove',['memmove',['../util_8h.html#a2d61e4c055b9e6c718bad21ada51ccdc',1,'util.h']]]
+];
