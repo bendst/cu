1f4b0a90a98cd3e82eb5801f7f9b52b59db9fdb3
Ben Stuart <mail@bstuart.de>
2016-04-05 18:46:52 +0200

    mainpage

---
+++ include/page.h
@@ -0,0 +1,21 @@
+/*! @mainpage About
+*
+*  	The CU Library contains various helper functions and datastructure.
+*
+* 	### Noteworthy
+* 	Most functions for the data structures are redefined in [util](util_8h.html)
+* 	as generic function using the c11 _Generic Macro.
+*
+*
+*  	## Usage
+*
+ *  ## Module
+ *  Following modules are provided:
+ *  - [dopply linked list](list_8h.html)
+ *  - [a continuous vector](vector_8h.html)
+ *  - [option type](option_8h.html)
+ *  - [various utility function](util_8h.html)
+ *
+ *
+ *
+ */
